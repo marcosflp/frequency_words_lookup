cd56fa5af25bcc0ed4a94993d2d3ccf41eea3e32
Marcos Felipe A. Ferreira <marcos.fel93@gmail.com>
2017-02-17 17:06:04 -0200

    WIP: Improve flask app
    - define a method that search for words in a response and return a list of words
    - add a test case to the search_word method
    - update requirements

--- tests.py
+++ tests.py
@@ -11,7 +11,10 @@ class ResponseCountWordsApiTest(unittest.TestCase):
         self.assertTrue(self.cls.is_validate_uri(uri))
 
     def test_count_words(self):
-        pass
+        # TODO: create an fixture to load an html response from a file
+        uri = 'http://flask-restful-cn.readthedocs.io/en/0.3.5/'
+        word = 'flask'
+        word_list = self.cls.search_word(uri, word)
 
 if __name__ == '__main__':
     unittest.main()
